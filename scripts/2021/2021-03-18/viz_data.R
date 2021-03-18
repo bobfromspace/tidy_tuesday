573da3c8ee5b33efb6eb6742246cc986d952e238
Nina Ilchenko <ilchnina@gmail.com>
2021-03-18 20:25:07 +0100

    last minute changes

--- scripts/2021/2021-03-18/viz_data.R
+++ scripts/2021/2021-03-18/viz_data.R
@@ -11,7 +11,7 @@ library(extrafont)
 #
 # Load data
 # --------------------------------------------------------------------------
-path <- "tidytuesday/data/2021/2021-03-18"
+path <- "tidytuesday/data/2021/2021-03-16"
 
 gms <- read.csv2(list.files(path, full.names = TRUE, pattern = "games"), sep = ",")
 
@@ -27,9 +27,11 @@ df_pic1 <- gms %>%
 	summarise(peak_sum = sum(peak / 1000000)) %>%
 	ungroup() %>%
 	arrange(desc(peak_sum)) %>%
-	slice(1L:10L) %>%
+	slice(1L:10L)
+
+df_pic1 <- df_pic1 %>%
 	mutate(gamename = factor(gamename,
-		levels = df_pic1$gamename[order(df_pic1$peak_sum, decreasing = TRUE)]))
+		levels = gamename[order(peak_sum, decreasing = TRUE)]))
 
 ggplot(df_pic1, aes(x = gamename, y = peak_sum, fill = gamename)) +
 	geom_bar(stat = "identity") +
@@ -38,7 +40,7 @@ ggplot(df_pic1, aes(x = gamename, y = peak_sum, fill = gamename)) +
 		panel.background = element_rect(fill = "#E5EDF7"),
 		legend.title.align = -0.1,
 		legend.title = element_text(margin = margin(t = 0, r = 0, b = 0, l = 5)),
-		legend.justification = c(1, 1.05),
+		legend.justification = c(1, 1.02),
 		legend.spacing.x = unit(0.4, "cm"),
 		plot.title.position = "plot",
 		plot.title = element_text(size = 14, margin = margin(t = 20, r = 20, b = 30, l = 8)),
@@ -46,7 +48,7 @@ ggplot(df_pic1, aes(x = gamename, y = peak_sum, fill = gamename)) +
 		axis.ticks = element_blank(),
 		axis.text.x = element_text(angle = -40, hjust = 0),
 		axis.text = element_text(colour = "#53647E"),
-		axis.text.y = element_text(margin = margin(t = 0, r = 30, b = 0, l = 15)),
+		axis.title.y = element_text(margin = margin(t = 0, r = 30, b = 0, l = 15)),
 		panel.grid.minor = element_blank(),
 		panel.grid.major.x = element_blank(),
 		text = element_text(family = "Verdana", size = 12, colour = "#53647E")
@@ -56,5 +58,5 @@ ggplot(df_pic1, aes(x = gamename, y = peak_sum, fill = gamename)) +
 		limits = c(0, max(df_pic1$peak_sum) + 5),
 		expand = c(0, 0))
 
-ggsave(file.path("viz", paste0("viz_", Sys.Date(), ".png")), width = ,
-	height = )
+ggsave(file.path("viz", paste0("viz_", Sys.Date(), ".png")), width = 9,
+	height = 7)
